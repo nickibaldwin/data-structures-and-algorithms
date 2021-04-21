0336ab23098a51b4f8d178890ea1380e59eb5170
Nicki Baldwin <nickibaldwin@gmail.com>
2021-04-20 21:13:57 -0700

    updated readme

--- code-challenges/arrayShift/arrayShift.js
+++ code-challenges/arrayShift/arrayShift.js
@@ -1,16 +1,16 @@
-const insertShiftArray = (arr, value) => {
+const insertShiftArray = (array, value) => {
     let arrayNew = [];
-    let middle = arr.length / 2;
-    if (arr.length % 2 !== 0) {
+    let middle = array.length / 2;
+    if (array.length % 2 !== 0) {
         middle += 0.5;
     }
-    for (let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < array.length; i++) {
         if (i < middle) {
-            arrayNew.push(arr[i]);
+            arrayNew.push(array[i]);
         } else if (i === middle) {
             arrayNew.push(value);
         } else if (i > middle) {
-            arrayNew.push(arr[i - 1]);
+            arrayNew.push(array[i - 1]);
         }
     }
     return arrayNew;
