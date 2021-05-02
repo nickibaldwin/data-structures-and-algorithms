120206d6b5d062cb5959d0ee1535382bcd226852
Nicki Baldwin <76967823+nickibaldwin@users.noreply.github.com>
2021-05-02 15:55:30 -0700

    test for cc9

---
+++ challenges401/llZip/README.md
@@ -0,0 +1,38 @@
+# Code Challenge Class 06 - Append Linked List
+
+### [Solution Code](challenges401/llZip/ll-zip.js)
+
+### [Test Code](challenges401/llZip/__test__/llZip.test.js)
+
+### [PR in Github](https://github.com/nickibaldwin/data-structures-and-algorithms/pull/22)
+
+### [Github Actions](https://github.com/nickibaldwin/data-structures-and-algorithms/actions)
+
+
+## Setup 
+
+Install proper dependencies:
+
+  `npm i jest`
+
+Test Application:
+
+  `npm run test llZip.tests.js`
+
+
+<!-- TODO:  -->
+## Challenge Summary
+
+Zip two linked lists.
+
+## Challenge Description
+
+Write a function called **zipLists** which takes two linked lists as arguments. Zip the two linked lists together into one so that the nodes alternate between the two lists and return a reference to the head of the zipped list. 
+  
+## Approach & Efficiency
+
+![](2021-04-28-20-08-53.png)
+
+### References:
+
+- [Udemy Master Class - Javascript Algorithms and Data Structures: Singly Likned List](https://www.udemy.com/course/js-algorithms-and-data-structures-masterclass/learn/lecture/8344200#overview)
---
+++ challenges401/llZip/linked-list.js
@@ -0,0 +1,79 @@
+'use strict';
+
+// const Node = require('./node.js');
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  insert(value){
+    let newNode = new Node(value);
+    if(!this.head){
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head; //newNode is now this.head
+      this.head = newNode;
+      this.length++;
+    }
+    return this;
+  }
+
+  includes(value) {
+    let current = this.head;
+    if(!this.head) return false;
+    while(current) {
+      if(current.value === value){
+        return true;
+      }
+      if(current !== current.next){
+        current = current.next;
+      }else {
+        current =null;
+      }
+    }
+    return false;
+  }
+
+  toString() {
+    let output = '';
+    let current = this.head;
+    if(!this.head) return output;
+    while(current) {
+      output += `{ ${current.value} } -> `;
+      current = current.next;
+    }
+    return output += 'NULL';
+  }
+
+  append(value) {
+    let newNode = new Node(value);
+    if(!this.head){
+      this.head = newNode;
+      return;
+    }
+    let current = this.head;
+    while(current.next) {
+      current = current.next;
+    }
+    current.next = newNode;
+  }
+
+}
+
+let newSinglyLinkedList = new SinglyLinkedList();
+newSinglyLinkedList.insert(5);
+newSinglyLinkedList.insert(10);
+newSinglyLinkedList.insert(15);
+
+module.exports = SinglyLinkedList;
